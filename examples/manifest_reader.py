89b057c6ed7377c578dbb19dae90b533a8e88ec1
Romain Thomas <romainthomasc@gmail.com>
2018-08-13 08:15:28 +0200

    Improve manfiest reader

--- examples/manifest_reader.py
+++ examples/manifest_reader.py
@@ -6,6 +6,10 @@ import pyAMI
 
 def main():
     parser = argparse.ArgumentParser(usage='%(prog)s [options] <App.apk | AndroidManifest.xml>')
+    parser.add_argument("-p", "--package-name", action="store_true", default=False)
+    parser.add_argument("-v", "--version", action="store_true", default=False)
+    parser.add_argument("-x", "--xml", action="store_true", default=False)
+    parser.add_argument("--perms", action="store_true", default=False)
     parser.add_argument("target",
             help='Apk or AndroidManifest.xml file')
 
@@ -17,7 +21,19 @@ def main():
         print("Unable to open '{}'".format(target))
         sys.exit(1)
 
-    print(manifest.xml)
+    if args.package_name:
+        print(manifest.package_name)
+
+    if args.version:
+        print("{} - {}".format(manifest.version_name, manifest.version_code))
+
+    if args.perms:
+        for p in manifest.permissions:
+            print(p)
+
+
+    if args.xml:
+        print(manifest.xml)
 
 
 if __name__ == "__main__":
